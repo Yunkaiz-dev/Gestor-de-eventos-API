8d901c4041db20b6107ceb80cd138d87971b0b80
Yunkaiz-dev <Vegarosariojoserafael@gmail.com>
2024-09-25 00:40:14 -0400

    Creacion de los DTOs

---
+++ DTO/Asistente/AsistenteDTO.cs
@@ -0,0 +1,17 @@
+﻿using Gestor_de_eventos.Modelos;
+
+namespace Gestor_de_eventos.DTO.Asistente
+{
+    public class AsistenteDTO
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public string Apellido { get; set; } = null!;
+
+        public string Telefono { get; set; } = null!;
+
+        public string Correo { get; set; } = null!;
+     }
+}
